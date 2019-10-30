1e8e8358bd637fbd3f19d12f385f202798fd5e1d
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2019-10-30 14:55:05 +0530

    Small Bug Occured

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-const app = require('express')();
-const cors = require('cors');
+const app = require("express")();
+const cors = require("cors");
 
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
-
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
 app.use(cors());
-app.use('*', cors())
+app.use("*", cors());
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'success'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success"
+  });
+});
 
+const port = process.env.PORT || 5000;
 
-const server = app.listen(process.env.PORT || 5000, () => {
-    console.log(`Main Server is listening for reqests in port ${port}....`)
-})
+const server = app.listen(port, () => {
+  console.log(`Main Server is listening for reqests on port ${port}....`);
+});
 
-const io = require('socket.io').listen(server);
+const io = require("socket.io").listen(server);
 
 // Emiting and Waiting for Evenst for Car
 
-io.on('connection', socket => {
-    console.log('Something get Connected');
-    
-    socket.on('straightmove', data => {
-        console.log('Sending data to car to move forward')
-        io.emit('forward', 'forward')
-    })
-    
-    socket.on('backmove', data => {
-        console.log('Sending data to car to move backward')
-        io.emit('backward', 'backward')
-    })
-    
-    socket.on('leftmove', data => {
-        console.log('Sending data to car to move left')
-        io.emit('left', 'left')
-    })
-    
-    socket.on('rightmove', data => {
-        console.log('Sending data to car to move right')
-        io.emit('right', 'right')
-    })
-})
+io.on("connection", socket => {
+  console.log("Something get Connected");
 
+  socket.on("straightmove", data => {
+    console.log("Sending data to car to move forward");
+    io.emit("forward", "forward");
+  });
+
+  socket.on("backmove", data => {
+    console.log("Sending data to car to move backward");
+    io.emit("backward", "backward");
+  });
+
+  socket.on("leftmove", data => {
+    console.log("Sending data to car to move left");
+    io.emit("left", "left");
+  });
+
+  socket.on("rightmove", data => {
+    console.log("Sending data to car to move right");
+    io.emit("right", "right");
+  });
+});
