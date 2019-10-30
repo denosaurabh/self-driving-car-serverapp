a03a8e91086fd3c38af3fb90047f3c522b9d314a
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2019-10-30 14:50:55 +0530

    Fixing Heroku Bug
    The bug was Bad gateway (503)

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const app = require('express')();
 const cors = require('cors');
 
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+
 app.use(cors());
 app.use('*', cors())
 
@@ -10,9 +14,8 @@ app.get('/', (req, res) => {
     })
 })
 
-const port = 5000;
 
-const server = app.listen(port, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
     console.log(`Main Server is listening for reqests in port ${port}....`)
 })
 
